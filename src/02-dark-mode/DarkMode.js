da68ac012ea054872159c03b88c286e79eb42843
Shaunak Chandra <79399986+Kingsky1t@users.noreply.github.com>
2023-03-22 20:11:13 +0000

    challenge 2 completed

--- src/02-dark-mode/DarkMode.js
+++ src/02-dark-mode/DarkMode.js
@@ -1,8 +1,22 @@
-export default function DarkMode () {
-  return (
-    <div className='page'>
-      <button className='dark-mode-button'>Dark Mode</button>
-      <button className='light-mode-button'>Light Mode</button>
-    </div>
-  )
+import { useState } from "react";
+export default function DarkMode() {
+     const [mode, setMode] = useState(false);
+     return (
+          <div className={`page ${mode ? "dark-mode" : null}`}>
+               <button
+                    className='dark-mode-button'
+                    onClick={() => {
+                         setMode(true);
+                    }}>
+                    Dark Mode
+               </button>
+               <button
+                    className='light-mode-button'
+                    onClick={() => {
+                         setMode(false);
+                    }}>
+                    Light Mode
+               </button>
+          </div>
+     );
 }
