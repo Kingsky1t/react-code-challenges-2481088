b27d6635bcaca9343e873244bb635b0c524e51ff
Shaunak Chandra <79399986+Kingsky1t@users.noreply.github.com>
2023-03-22 20:51:28 +0000

    challenge 4 completed

--- src/04-dog-pics/DogPics.js
+++ src/04-dog-pics/DogPics.js
@@ -1,9 +1,22 @@
-export default function DogPics () {
-  // API: https://dog.ceo/dog-api/
-  return (
-    <div className='dog-pics'>
-      <img src='https://images.dog.ceo/breeds/spaniel-cocker/n02102318_4172.jpg' />
-      <button>🐶</button>
-    </div>
-  )
+import { useState, useEffect } from "react";
+const getDogPic = async () => {
+     const response = await fetch("https://dog.ceo/api/breeds/image/random");
+     const dog = await response.json();
+     return dog.message;
+};
+export default function DogPics() {
+     const [imageUrl, setImageUrl] = useState("");
+     useEffect(() => {
+          getDogPic().then((url) => setImageUrl(url));
+     }, []);
+
+     return (
+          <div className='dog-pics'>
+               <img src={imageUrl} />
+
+               <button onClick={async (e) => setImageUrl(await getDogPic())}>
+                    🐶
+               </button>
+          </div>
+     );
 }
