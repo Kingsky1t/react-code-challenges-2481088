a05079ac4f5a3afff3c10a0f49cbeb70f60b2b4a
Shaunak Chandra <79399986+Kingsky1t@users.noreply.github.com>
2023-03-23 06:14:49 +0000

    challenge 6 completed

--- src/06-add-window-event/WindowEvent.js
+++ src/06-add-window-event/WindowEvent.js
@@ -1,5 +1,14 @@
-export default function WindowEvent () {
-  return (
-    <h2>Window event active</h2>
-  )
+import { useEffect } from "react";
+
+export default function WindowEvent() {
+     const action = () => {
+          alert("mouse pressed");
+     };
+     useEffect(() => {
+          window.addEventListener("dblclick",action);
+          return () => {
+               window.removeEventListener("dblclick",action);
+          };
+     }, []);
+     return <h2>Window event active</h2>;
 }
