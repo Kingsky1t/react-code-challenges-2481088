0d32fca872f1da39cc1396650e23bc135cc3058e
Shaunak Chandra <79399986+Kingsky1t@users.noreply.github.com>
2023-03-23 06:31:25 +0000

    challenge 7 completed

--- src/07-color-picker/Color.js
+++ src/07-color-picker/Color.js
@@ -1,8 +1,9 @@
-export default function Color ({ hex, name }) {
+export default function Color ({ hex, name, handleClick }) {
   return (
     <button
       className='color-square'
       style={{ backgroundColor: hex }}
+      onClick={()=>{handleClick(hex)}}
     >
       <h2>{name}</h2>
     </button>
